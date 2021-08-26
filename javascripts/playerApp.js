c320e7868384ca5817b6c9896ebee8fc58975946
netscout <netscout82@naver.com>
2021-08-26 16:28:47 +0900

    테스트 추가

--- javascripts/playerApp.js
+++ javascripts/playerApp.js
@@ -1,54 +1,54 @@
 var appUi = new Vue({
-    el: '#app',
-    data: {
-        url: "",
-        container: null,
-        userName: "사용자",
-        userId: "user"
-    },
-    mounted: function () {
-        player = videojs('preview-player', {
-            sources: [{
-                src: this.url,
-                type: 'application/x-mpegURL'
-              }],
-            videoWidth: 1080,
-            videoHeight: 1920,
-            autoplay: true
-        });
-        
-        // 채팅 영역 추가
-        this.container = document.getElementById("preview-player"); 
-        const chat = document.createElement("div");
-        chat.id = "viewport";
-        
-        chat.classList.add("chat");
-        this.container.append(chat);
-    },
-    created: function () {
-        console.log('Ready to start');
-        let urlParams = new URLSearchParams(window.location.search);
-        this.url = urlParams.get('url');
-        console.log(this.url);
+  el: "#app",
+  data: {
+    url: "",
+    container: null,
+    userName: "사용자",
+    userId: "user",
+  },
+  mounted: function () {
+    player = videojs("preview-player", {
+      sources: [
+        {
+          src: this.url,
+          type: "application/x-mpegURL",
+        },
+      ],
+      //   videoWidth: 1080,
+      //   videoHeight: 1920,
+      autoplay: true,
+    });
 
-        let userId = localStorage.getItem('userId');
-        let userName = localStorage.getItem('userName');
-        if(!userId) {
-            const randNum = rand_0_100000();
+    // 채팅 영역 추가
+    // this.container = document.getElementById("preview-player");
+    // const chat = document.createElement("div");
+    // chat.id = "viewport";
 
-            userId = `${this.userId}${randNum}`;
-            userName = `${this.userName}${randNum}`;
+    // chat.classList.add("chat");
+    // this.container.append(chat);
+  },
+  created: function () {
+    console.log("Ready to start");
+    let urlParams = new URLSearchParams(window.location.search);
+    this.url = urlParams.get("url");
+    console.log(this.url);
 
-            localStorage.setItem('userId', userId);
-            localStorage.setItem('userName', userName);
-        }
-        
-        this.userId = userId;
-        this.userName = userName;
-        console.log(this.userId, this.userName);
-    },
-    methods: {
-    },
-    computed: {
-    }
+    // let userId = localStorage.getItem('userId');
+    // let userName = localStorage.getItem('userName');
+    // if(!userId) {
+    //     const randNum = rand_0_100000();
+
+    //     userId = `${this.userId}${randNum}`;
+    //     userName = `${this.userName}${randNum}`;
+
+    //     localStorage.setItem('userId', userId);
+    //     localStorage.setItem('userName', userName);
+    // }
+
+    // this.userId = userId;
+    // this.userName = userName;
+    // console.log(this.userId, this.userName);
+  },
+  methods: {},
+  computed: {},
 });
